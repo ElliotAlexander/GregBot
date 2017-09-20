7f162f7ea8ec9f4293304592dafeb46ebb7c50a6
ElliotAlexander <epurvis97@gmail.com>
2017-09-20 01:51:26 +0100

    Updated start script.

--- running_example/gregBotStart.sh
+++ running_example/gregBotStart.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-echo Starting Gregbot...
+echo Starting Twitter-Picture-Bot...
 if [ -f logfile.txt ]
 then
 	echo Moving old logfile.txt into logfile_old.txt....
 	mv logfile.txt logfile_old.txt
 fi
 
-cd ~
-cd GregBot/
-screen -dmS GregBot bash -c 'java -jar GregBot-1.0-SNAPSHOT.jar | tee logfile.txt'
-if   ! screen -ls | grep -q "GregBot"; then
-	echo ERROR - GregBot failed to initialise.	
+screen -dmS Twitter-Picture-Bot bash -c 'java -jar Twitter-Picture-Bot-1.0-SNAPSHOT.jar | tee logfile.txt'
+if   ! screen -ls | grep -q "Twitter-Picture-Bot"; then
+	echo ERROR - Twitter-Picture-Bot failed to initialise.	
 	exit 0
 fi
-echo Gregbot started successfully!
+echo Twitter-Picture-Bot started successfully!
